f44a09db976460f45cfe1bb9c960c957e483a783
shakil51298 <zikaiwen02@gmail.com>
2021-02-12 22:49:05 +0800

    typescript

--- distribute/first.js
+++ distribute/first.js
@@ -1,3 +1,18 @@
 "use strict";
 let MyMoney = 50;
 console.log(MyMoney);
+function Add(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+const FullName = Add('shakil', 'khan');
+console.log(FullName);
+function doubbleIt(number) {
+    return number * 2;
+}
+const douleValue = doubbleIt(5);
+console.log(douleValue);
+const muliply = (x, y) => x * y;
+console.log(5 * 6);
+const Numbers = [1, 2, 3, 4, 5, 6];
+Numbers.push(22);
+console.log(Numbers);
--- first.ts
+++ first.ts
@@ -1,2 +1,24 @@
 let MyMoney = 50;
 console.log(MyMoney);
+
+function Add(firstName:string,lastName:string):string {
+    return firstName + " " + lastName;
+}
+
+const FullName = Add('shakil','khan');
+console.log(FullName);
+
+function doubbleIt(number:number) : number{
+    return number * 2;
+}
+const douleValue = doubbleIt(5);
+console.log(douleValue);
+
+const muliply = (x : number, y: number)=> x * y;
+console.log(5*6);
+
+const Numbers = [1 , 2, 3, 4, 5, 6];
+Numbers.push(22);
+console.log(Numbers);
+
+
